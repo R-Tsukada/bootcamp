12d9ca62c4bc6b75d287dcad05a648681e2e0ffd
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-07-28 17:58:32 +0900

    resourcesを使用しroutesを設定しました

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,6 @@ Rails.application.routes.draw do
   get "pricing", to: "welcome#pricing", as: "pricing"
   get "training", to: "welcome#training", as: "training"
   get "faq", to: "welcome#faq", as: "faq"
-  get "graduation_works", to: "welcome#graduation_works", as: "graduation_works"
-  get "graduation_works/:id", to: "welcome#graduation_works_show"
   get "tos", to: "welcome#tos", as: "tos"
   get "pp", to: "welcome#pp", as: "pp"
   get "law", to: "welcome#law", as: "law"
@@ -167,7 +165,7 @@ Rails.application.routes.draw do
     end
   end
   resources :works, except: %i(index)
-
+  resources :graduation_works
   resources :questions, only: %i(index show new create destroy)
   resources :courses, only: :index
 
